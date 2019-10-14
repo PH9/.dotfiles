d66cc841d2786e67d7c201a2d4b2915515b40e18
Wasith Theerapattrathamrong <phai@live.com>
2019-10-14 22:29:13 +0700

    Announce every quarter hour

--- macos/macos.sh
+++ macos/macos.sh
@@ -24,3 +24,8 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Announce every quarter hour with Deniel sound
+defaults write com.apple.speech.synthesis.general.prefs TimeAnnouncementPrefs -dict TimeAnnouncementsEnabled -bool YES \
+    TimeAnnouncementsIntervalIdentifier -string EveryQuarterHourInterval \
+    TimeAnnouncementsVoiceSettings '{ VoiceIdentifier = "com.apple.speech.synthesis.voice.daniel.premium"; }'
