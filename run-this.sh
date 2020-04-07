c5f95bd8c7d26261f671569d87a715b2dffc1205
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-04-07 20:45:24 +0700

    Install rbenv, then xcode-install, then brew that contains swiftlint that needed xcode full version

--- run-this.sh
+++ run-this.sh
@@ -9,16 +9,14 @@ else
   echo '[I] Homebrew is already install, skip installation.'
 fi
 
+# brew setup
 
 brew update
 brew upgrade
 
-brew bundle
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Setup Ruby
+brew install rbenv
+
 ruby_version=2.7.0
 
 rbenv install $ruby_version
@@ -31,7 +29,25 @@ if grep 'eval "$(rbenv init -)"' "$HOME/.zshrc" >/dev/null; then
 else
     echo "[!] Setting rbenv to ~/.zshrc"
     ex -sc '1i|eval "$(rbenv init -)"' -cx $HOME/.zshrc
+    eval "$(rbenv init -)"
 fi
+
+gem install bundler
+bundle update
+
+xcode_version=11.4
+
+xcversion update
+xcversion list
+xcversion install $xcode_version
+xcversion select $xcode_version --symlink
+
+# Continue with brew since some dependency needed full version of xcode
+
+brew bundle
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 gem install bundler
 bundle install
