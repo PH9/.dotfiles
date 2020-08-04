d6800d886d50013d1a4dee4c971bb77eee69e58a
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-08-04 12:54:44 +0700

    Copy some from https://github.com/holman/dotfiles

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,3 +1,15 @@
+# Sets reasonable macOS defaults.
+#
+# Or, in other words, set shit how I like in macOS.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
