60258e56ae0695703508d2c0340b074637f5afcc
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2020-08-05 12:08:00 +0700

    Fix: cannot execute

---
+++ ruby/install.sh
@@ -0,0 +1,12 @@
+# Setup Ruby Environment
+# Set global to use version 2.6.6 (Mainly to move it away from system's version of Ruby)
+echo "Installing Ruby"
+eval "$(rbenv init -)"
+rbenv install 2.6.6
+rbenv global 2.6.6
+ruby --version
+
+# Install and link bundle with rbenv
+echo "Installing bundler"
+gem install bundler
+ruby -r bundler -e "puts RUBY_VERSION"
