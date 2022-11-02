e6fb74dfa96be60bb33a3b3e45f1f25503834898
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2022-11-02 21:25:58 +0700

    fix: GOROOT

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,12 @@
 export GOPATH=$HOME/go
-export PATH="$PATH:$GOPATH/bin"
+export PATH="$GOPATH/bin:$PATH"
+
+export GOROOT="$(brew --prefix golang)/libexec"
+
+if [[ $PATH != *$GOPATH* ]]; then
+  export PATH="${GOPATH}/bin:${PATH}"
+fi
+
+if [[ $PATH != *$GOROOT* ]]; then
+  export PATH="${GOROOT}/bin:${PATH}"
+fi
