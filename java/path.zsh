c75e10598ca10d7e2076e5e56f5cc8a58c3045d1
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2023-07-10 13:30:55 +0700

    Use intellij to handle java

--- java/path.zsh
+++
@@ -1,6 +0,0 @@
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-PATH="$HOME/.jenv/bin:$PATH"
-export JAVA_HOME=$(/usr/libexec/java_home)
-
-# echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-# export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
